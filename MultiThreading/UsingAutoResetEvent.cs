582f5e6db569c492f1c02f5528ce1926a7ad8638
jonathancoombes <coombes@web.de>
2020-12-10 07:06:28 +0200

    Added AutoResetEvent Sample

---
+++ MultiThreading/UsingAutoResetEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreading
+{
+    public class UsingAutoResetEvent
+    {
+        AutoResetEvent _autoReset = new AutoResetEvent(true);
+        public UsingAutoResetEvent()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                new Thread(FileWriter).Start();
+            }
+        }
+
+        public void FileWriter()
+        {
+            _autoReset.WaitOne();
+            Console.WriteLine("Writing process started by Thread: #{0}", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(5000);
+            Console.WriteLine("Writing process finished");
+            _autoReset.Set();
+        }
+
+    }
+}
